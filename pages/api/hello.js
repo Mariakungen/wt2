cedaeb8ec245cf1e2737cf62364db3e075efe74f
Maria Olsson <mo223id@student.lnu.se>
2022-04-28 00:43:54 +0200

    works on localhost

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { Client } from '@elastic/elasticsearch'
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   console.log('hello')
   const client = elasticClient()
   const cocoaData = query()
