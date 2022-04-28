e1cc19a259013f17e3a66b4c7f00b38dfabb7084
Maria Olsson <mo223id@student.lnu.se>
2022-04-28 13:55:47 +0200

    apexvis tries

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,44 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+// import ApexCharts from 'apexcharts'
+import dynamic from 'next/dynamic'
+const ApexCharts = dynamic(() => import('react-apexcharts'), { ssr: false });
 
-export default function Home({ data }) {
-  //skriva js
+export default function Home() {
+  var options = {
+    chart: {
+      type: 'bar'
+    },
+    series: [
+      {
+        name: 'sales',
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
+      }
+    ],
+    xaxis: {
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+    }
+  }
+  
+  var chart = new ApexCharts(document.querySelector('#chart'), options)
+
+  chart.render()
+  
   return (
-    <div>Hello Pauliina</div>
-  ) // skriva html
+    <div id="chart">
+    
+      <style jsx>{`
+        body {
+          font-family: Roboto, sans-serif;
+        }
+        
+        #chart {
+          max-width: 650px;
+          margin: 35px auto;
+        }
+        
+      `}</style>
+    </div>
+  );
 }
 
 export async function getServerSideProps() {
